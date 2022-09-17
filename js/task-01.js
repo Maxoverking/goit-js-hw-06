3aa0c6529c76252128e4ba4282a09bf5877dbd14
Maxoverking <avemax7@gmail.com>
2022-09-18 01:17:42 +0200

    первая задача

--- js/task-01.js
+++ js/task-01.js
@@ -2,15 +2,16 @@ const numberOfCategory = document.querySelectorAll('.item');
 
 console.log('Number of categories:', numberOfCategory.length);
 
-
 const category = document.querySelectorAll('h2');
 
+const r = numberOfCategory[0].querySelectorAll('li');
 console.log("Category :", category[0].textContent);
+console.log("Elements:", r.length);
+
+const q = numberOfCategory[1].querySelectorAll('li');
 console.log("Category :", category[1].textContent);
+console.log("Elements:", q.length);
+
+const w = numberOfCategory[2].querySelectorAll('li');
 console.log("Category :", category[2].textContent);
-
-
-
-
-// const menuItemsByTagName = document.querySelectorAll("li");
-// console.log(menuItemsByTagName);
+console.log("Elements:", w.length);
