f603b04c6c319bf2d14c11790ba95ae9077739fc
Maxoverking <avemax7@gmail.com>
2022-09-17 23:27:13 +0200

    первый комит

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const numberOfCategory = document.querySelectorAll('.item');
+
+console.log('Number of categories:', numberOfCategory.length);
+
+
+const category = document.querySelectorAll('h2');
+
+console.log("Category :", category[0].textContent);
+console.log("Category :", category[1].textContent);
+console.log("Category :", category[2].textContent);
+
+
+
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
