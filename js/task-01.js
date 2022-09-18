64ad5ff27bd55e6d3fb2a1e14c4bec958bbb9468
Maxoverking <avemax7@gmail.com>
2022-09-18 14:53:12 +0200

    первое задание

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,37 @@
-const numberOfCategory = document.querySelectorAll('.item');
+// const numberOfCategory = document.querySelectorAll('.item');
+// console.log('Number of categories:', numberOfCategory.length);
 
+// // console.log('Number of categories:', numberOfCategory);
+// const category = document.querySelectorAll('h2');
+
+// const el_1 = numberOfCategory[0].querySelectorAll('li');
+// console.log("Category :", category[0].textContent);
+// console.log("Elements:", el_1.length);
+
+// const el_2 = numberOfCategory[1].querySelectorAll('li');
+// console.log("Category :", category[1].textContent);
+// console.log("Elements:", el_2.length);
+
+// const el_3 = numberOfCategory[2].querySelectorAll('li');
+// console.log("Category :", category[2].textContent);
+// console.log("Elements:", el_3.length);
+//=====================================================
+
+const numberOfCategory = document.querySelectorAll(".item");
 console.log('Number of categories:', numberOfCategory.length);
 
-const category = document.querySelectorAll('h2');
+const categoriesArray = [...numberOfCategory].map( elem => elem.children[0].textContent);
 
-const r = numberOfCategory[0].querySelectorAll('li');
-console.log("Category :", category[0].textContent);
-console.log("Elements:", r.length);
+const elementsArray = [...numberOfCategory].map( elem => elem.children[1].children.length);
 
-const q = numberOfCategory[1].querySelectorAll('li');
-console.log("Category :", category[1].textContent);
-console.log("Elements:", q.length);
+console.log("Category :",categoriesArray[0]);
+console.log(`Elements: `, elementsArray[0] );
+console.log("Category :",categoriesArray[1]);
+console.log(`Elements: `, elementsArray[1] );
+console.log("Category :",categoriesArray[2]);
+console.log(`Elements: `, elementsArray[2] );
 
-const w = numberOfCategory[2].querySelectorAll('li');
-console.log("Category :", category[2].textContent);
-console.log("Elements:", w.length);
+//================================================================
+
+
+
