be2bfd90a02417cb9a7f170b3e3016d85834baf8
Maxoverking <avemax7@gmail.com>
2022-09-18 15:42:19 +0200

    первое задание другое решение

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,29 @@
+//Variant 1
+
+
+const numberOfCategory = document.querySelectorAll(".item");
+console.log('Number of categories:', numberOfCategory.length);
+
+const categoriesArray = [...numberOfCategory].map(elem => elem.children[0].textContent);
+
+console.log("categoriesArray", categoriesArray)
+
+const elementsArray = [...numberOfCategory].map(elem => elem.children[1].children.length);
+
+console.log("Category :",categoriesArray[0]);
+console.log(`Elements: `, elementsArray[0] );
+console.log("Category :",categoriesArray[1]);
+console.log(`Elements: `, elementsArray[1] );
+console.log("Category :",categoriesArray[2]);
+console.log(`Elements: `, elementsArray[2] );
+
+
+
+
+//================================================================
+//Variant 2
+
+
 // const numberOfCategory = document.querySelectorAll('.item');
 // console.log('Number of categories:', numberOfCategory.length);
 
@@ -15,23 +41,4 @@
 // const el_3 = numberOfCategory[2].querySelectorAll('li');
 // console.log("Category :", category[2].textContent);
 // console.log("Elements:", el_3.length);
-//=====================================================
-
-const numberOfCategory = document.querySelectorAll(".item");
-console.log('Number of categories:', numberOfCategory.length);
-
-const categoriesArray = [...numberOfCategory].map( elem => elem.children[0].textContent);
-
-const elementsArray = [...numberOfCategory].map( elem => elem.children[1].children.length);
-
-console.log("Category :",categoriesArray[0]);
-console.log(`Elements: `, elementsArray[0] );
-console.log("Category :",categoriesArray[1]);
-console.log(`Elements: `, elementsArray[1] );
-console.log("Category :",categoriesArray[2]);
-console.log(`Elements: `, elementsArray[2] );
-
-//================================================================
-
-
 
